8e8aac257c9904d2120afa1c9136b2ba40d1e929
raaj6395 <mrraaj6395@gmail.com>
2024-03-19 11:07:03 +0530

    error fix

--- src/pages/OrderStatusPage.tsx
+++ src/pages/OrderStatusPage.tsx
@@ -22,10 +22,12 @@ const OrderStatusPage = () => {
           <div className="grid gap-10 md:grid-cols-2">
             <OrderStatusDetail order={order} />
             <AspectRatio ratio={16 / 5}>
-              <img
+              if(order.restaurant?.imageUrl){
+                <img
                 src={order.restaurant.imageUrl}
                 className="rounded-md object-cover h-full w-full"
               />
+              }
             </AspectRatio>
           </div>
         </div>
