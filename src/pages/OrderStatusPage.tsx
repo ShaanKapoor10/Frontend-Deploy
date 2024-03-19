c853ffe1873c5222c7e75f6763f12f78f2ac89ba
Shaan Kapoor <124860688+ShaanKapoor10@users.noreply.github.com>
2024-03-19 12:57:32 +0530

    Update OrderStatusPage.tsx

--- src/pages/OrderStatusPage.tsx
+++ src/pages/OrderStatusPage.tsx
@@ -22,12 +22,12 @@ const OrderStatusPage = () => {
           <div className="grid gap-10 md:grid-cols-2">
             <OrderStatusDetail order={order} />
             <AspectRatio ratio={16 / 5}>
-              if(order.restaurant?.imageUrl){
+              
                 <img
                 src={order.restaurant.imageUrl}
                 className="rounded-md object-cover h-full w-full"
               />
-              }
+              
             </AspectRatio>
           </div>
         </div>
